806e4abd901b27fe77f1d822f468be2fe464e9b7
Ihor Chekhunov <bimbaba@Ihors-MacBook-Pro.local>
2023-07-11 22:20:48 +0200

    [Feature] example

---
+++ app/hooks/useCountries.ts
@@ -0,0 +1,22 @@
+import { useQuery } from "react-query";
+import { CountryService, ICountry } from "../services/country.service";
+
+export const useCountries = () => {
+  const {
+    isLoading,
+    data: countries,
+    refetch,
+  } = useQuery("country list", () => CountryService.getAll(), {
+    onError: (error: any) => {
+      alert(error.message);
+    },
+    select: ({ data }): ICountry[] =>
+      data.map((country) => ({
+        ...country,
+        title: country.title + " !",
+      })),
+    enabled: false,
+  });
+
+  return { isLoading, countries, refetch };
+};
